0268c5fd450a4920c1de9ae49ac007da0a3ef9f2
yu0428 <cbyrr1@gmail.com>
2015-03-16 22:53:31 +0800

    assignment_pingtai.sql contains the database definition statements.

---
+++ assignment_pingtai.sql
@@ -0,0 +1,11 @@
+create database assignment_pingtai;
+
+use assignment_pingtai;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `name` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `image` blob,
+  PRIMARY KEY (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
